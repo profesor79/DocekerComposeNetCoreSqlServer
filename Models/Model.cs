29b90e8568edcad14ced1985f57ebaf4b103c0d2
profesor <profesor@DESKTOP-24S54T6>
2018-01-06 23:37:02 +0000

    trying to get ef working

---
+++ Models/Model.cs
@@ -0,0 +1,36 @@
+// source: https://ef.readthedocs.io/en/staging/platforms/aspnetcore/new-db.html
+// then used this one as previous was a bit outdated http://www.learnentityframeworkcore.com/walkthroughs/aspnetcore-application
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
