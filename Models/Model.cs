9de2373aa07f09b4d8bbe3797d45d94625b78ea8
profesor <profesor@DESKTOP-24S54T6>
2018-01-06 23:48:56 +0000

    added firs migration

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,16 +1,51 @@
 // source: https://ef.readthedocs.io/en/staging/platforms/aspnetcore/new-db.html
 // then used this one as previous was a bit outdated http://www.learnentityframeworkcore.com/walkthroughs/aspnetcore-application
 
+/*
+ dotnet ef migrations list
+Unable to create an object of type 'BloggingContext'. Add an implementation of 'IDesignTimeDbContextFactory<BloggingContext>' to the project, 
+or see https://go.microsoft.com/fwlink/?linkid=851728 for additional patterns supported at design time.
+
+ */
+
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Data.SqlClient;
 
 namespace EFGetStarted.AspNetCore.NewDb.Models
 {
+
+     public class BloggingContextFactory : IDesignTimeDbContextFactory<BloggingContext>
+    {
+        public BloggingContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BloggingContext>();
+            
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();            
+            builder.DataSource = "sqlexpress"; 
+            builder.UserID = "sa";            
+            builder.Password = "tota11y5ecret";     
+            builder.InitialCatalog = "excercise";                        
+            optionsBuilder.UseSqlServer(builder.ConnectionString);
+
+            return new BloggingContext(optionsBuilder.Options);
+        }
+    }
+    
+
+
+
     public class BloggingContext : DbContext
     {
         public BloggingContext(DbContextOptions<BloggingContext> options)
             : base(options)
         { }
+
+        public BloggingContext(DbContextOptions options) : base(options)
+{
+}
+
 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
