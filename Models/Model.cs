0e4fd97f8593f44d9757e71976c44e9fbbe56682
profesor <profesor@DESKTOP-24S54T6>
2018-01-07 00:01:28 +0000

    added seed data

--- Models/Model.cs
+++ Models/Model.cs
@@ -2,9 +2,12 @@
 // then used this one as previous was a bit outdated http://www.learnentityframeworkcore.com/walkthroughs/aspnetcore-application
 
 /*
- dotnet ef migrations list
+dotnet ef migrations list
 Unable to create an object of type 'BloggingContext'. Add an implementation of 'IDesignTimeDbContextFactory<BloggingContext>' to the project, 
 or see https://go.microsoft.com/fwlink/?linkid=851728 for additional patterns supported at design time.
+---------------
+add some test data 
+https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro#add-code-to-initialize-the-database-with-test-data
 
  */
 
@@ -12,6 +15,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore.Design;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace EFGetStarted.AspNetCore.NewDb.Models
 {
@@ -68,4 +72,46 @@ namespace EFGetStarted.AspNetCore.NewDb.Models
         public int BlogId { get; set; }
         public Blog Blog { get; set; }
     }
+
+    public static class DbInitializer
+    {
+        public static void Initialize(BloggingContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Blogs.Any())
+            {
+                return;   // DB has been seeded
+            }
+            var blogs = new Blog[]{
+            new Blog{BlogId=1},
+            new Blog{BlogId=2},
+            new Blog{BlogId=3},
+            };
+
+
+            if (context.Posts.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var posts = new Post[]{
+                new Post{PostId= 1, Title="Greg meets Docker", Content = "123", BlogId=1},
+                new Post{PostId= 2, Title="Joe meets Docker", Content = "123", BlogId=1},
+                new Post{PostId= 3, Title="Dave meets apple", Content = "123", BlogId=1},
+                new Post{PostId= 4, Title="Who wants banana", Content = "124", BlogId=1},
+            };
+
+            context.SaveChanges();
+            
+
+
+
+
+            context.SaveChanges();
+        }
+    }
 }
+
+
+
